3ad981283027f6a507c8e6c6a804670c2e38941a
John Daryl Homecillo <54143259+hmcldryl@users.noreply.github.com>
2024-11-16 02:32:48 +0800

    Remove custom deploy configs

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? '/me/' : '',
-  basePath: isProd ? '/me' : '',
-  output: 'export'
+  /* config options here */
 };
 
 export default nextConfig;
